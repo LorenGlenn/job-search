71e35dc6551f482bec826c098955df4ae3e6c942
loren glenn <lorencglenn@gmail.com>
2017-03-06 10:52:14 -0800

    add more apis, indeed and glassdoor

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,6 +6,8 @@ class SearchesController < ApplicationController
 
   def create
     @dice = HTTParty.get('http://service.dice.com/api/rest/jobsearch/v1/simple.json?text=' + params["New Search"][:query])
+    @glassdoor = HTTParty.get('http://api.glassdoor.com/api/api.htm?t.p=129477&t.k=g01DzFNPdZw&userip=0.0.0.0&useragent=test&format=json&v=1&action=jobs-stats&returnJobTitles=true&q=' + params["New Search"][:query])
+    @indeed = HTTParty.get('http://api.indeed.com/ads/apisearch?publisher=7176344717561984&v=2&limit=20&q=' + params["New Search"][:query])
     render :show
   end
 end

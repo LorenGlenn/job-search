2dd013ac91e4c50d2f881f9b43b96c6dfbc3d8ae
loren glenn <lorencglenn@gmail.com>
2017-03-06 09:54:54 -0800

    add dice api

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,11 @@
 class SearchesController < ApplicationController
+
+  def index
+    @search = "New Search"
+  end
+
+  def create
+    @dice = HTTParty.get('http://service.dice.com/api/rest/jobsearch/v1/simple.json?text=' + params["New Search"][:query])
+    render :show
+  end
 end

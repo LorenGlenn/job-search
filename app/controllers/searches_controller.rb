536cf5f62cbe3f2194b94de1c6277e3ddbdd5f5c
Loren Glenn <lorencglenn@gmail.com>
2017-03-07 14:52:07 -0800

    add ajax to api-calls

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,20 +5,27 @@ class SearchesController < ApplicationController
   end
 
   def show
-    query = params[:query]
-    diceResponse = ApiService::Dice.new(query)
-    @dice = diceResponse.get_dice
-
-    indeedResponse = ApiService::Indeed.new(query)
-    @indeed = indeedResponse.get_indeed
-
-    glassdoorResponse = ApiService::Glassdoor.new(query)
-    @glassdoor = glassdoorResponse.get_glassdoor
-
-    githubResponse = ApiService::Github.new(query)
-    @github = githubResponse.get_github
-
+    @query = params[:query]
+    respond_to do |format|
+      if params[:dice]
+        diceResponse = ApiService::Dice.new(@query)
+        @dice = diceResponse.get_dice
+        format.js { render 'dice' }
+      elsif params[:indeed]
+        indeedResponse = ApiService::Indeed.new(@query)
+        @indeed = indeedResponse.get_indeed
+        format.js { render 'indeed' }
+      elsif params[:glassdoor]
+        glassdoorResponse = ApiService::Glassdoor.new(@query)
+        @glassdoor = glassdoorResponse.get_glassdoor
+        format.js { render 'glassdoor' }
+      elsif params[:github]
+        githubResponse = ApiService::Github.new(@query)
+        @github = githubResponse.get_github
+        format.js { render 'github' }
+      else
+        format.html
+      end
+    end
   end
-
-
 end

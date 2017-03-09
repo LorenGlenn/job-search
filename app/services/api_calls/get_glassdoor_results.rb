465f507802a0eb8a870a033f13dafb6c5b9c8639
loren glenn <lorencglenn@gmail.com>
2017-03-08 16:27:52 -0800

    add animation

--- app/services/api_calls/get_glassdoor_results.rb
+++ app/services/api_calls/get_glassdoor_results.rb
@@ -8,9 +8,13 @@ class Glassdoor
   end
 
   def get_glassdoor
+    begin
     glassdoor = HTTParty.get('http://api.glassdoor.com/api/api.htm?t.p=129477&t.k=g01DzFNPdZw&userip=0.0.0.0&useragent=test&format=json&v=1&action=jobs-stats&returnCities=true&returnJobTitles=true&q=' + @query+'&city='+@city+'&state='+@state)
     response = glassdoor["response"]["cities"]
     response
+    rescue
+      puts 'get_glassdoor'
+    end
   end
 end
 end
